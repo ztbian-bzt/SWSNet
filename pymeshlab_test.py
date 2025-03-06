bb5cffe09023177dd22f9b41345e92f0cbf018c5
ztbian-bzt <155946045+ztbian-bzt@users.noreply.github.com>
2025-03-06 19:00:54 +0800

    Delete pymeshlab_test.py

--- pymeshlab_test.py
+++
@@ -1,7 +0,0 @@
-import pymeshlab as ml
-
-mesh = ml.MeshSet()
-mesh.load_new_mesh('Unsectioned_LowerJaw.stl')
-mesh.load_new_mesh('Tooth_{30_94666_20240602_1358}.stl')
-mesh.generate_boolean_intersection(first_mesh=0, second_mesh=1)
-mesh.save_current_mesh('intersection.stl')
